951f7d03bb4605b94069df50eca5796671415a78
paul1897 <chillaganajonathan@gmail.com>
2025-09-30 20:03:11 -0500

    Cambio

--- src/Paginacion.tsx
+++ src/Paginacion.tsx
@@ -1,14 +1,11 @@
 
 
-import React from 'react';
-
 
 interface PaginacionProps {
   totalPages: number;
   currentPage: number;
   goToPage: (page: number) => void;
 }
-
 const Paginacion = ({ totalPages, currentPage, goToPage }: PaginacionProps) => {
   if (totalPages <= 1) return null; // no mostrar si solo hay 1 página
 
